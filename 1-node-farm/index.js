cb2d3f5c4e0e75ba47aa74bb5d3f1659e00dd341
TLaca <tlacanilao@marylane.eu>
2024-12-17 19:21:41 +0100

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,52 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(textIn);
+////////////////////////////////////////////////////
+// FILES
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('txt/output.txt', textOut);
-console.log('File written');
+// // Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('txt/output.txt', textOut);
+// console.log('File written');
+
+// // Non-Blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error! 🚨')
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//       console.log(data2);
+//       fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//         console.log(data3);
+
+//         fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'Utf-8', err => {
+//           console.log('Your file is written!👌');
+//         })
+//     })
+//   })
+// })
+// console.log('Will read file!')
+
+////////////////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  
+  if (pathName == '/' || pathName == '/overview') {
+    res.end('This is the OVERVIEW!');
+  } else if (pathName == '/product') {
+    res.end('This is the PRODUCT!');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html', 
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});
